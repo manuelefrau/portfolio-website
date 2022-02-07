27f1750361cd874ce4789f1d60fd73f93dd67a88
manuelefrau <98127538+manuelefrau@users.noreply.github.com>
2022-02-07 23:56:59 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
+  if (x.className === 'navtoggle' && $(window).width() < 540) {
     x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
